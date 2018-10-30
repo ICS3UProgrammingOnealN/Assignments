d96a5c6272c34fe6f2806758c10c46e79a5fb1ee
OnealNnah <onealnnah87@gmail.com>
2018-10-30 15:00:46 -0400

    Initial commit

---
+++ FallingObjectsOneal/FallingObjects/FallingObjects/FallingObjectForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace FallingObjects
+{
+    partial class FallingObjectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblHeigth = new System.Windows.Forms.Label();
+            this.txtHeigth = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(299, 148);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(130, 54);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(52, 37);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(611, 29);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "Enter the time (in second) since you released the Object";
+            this.lblTime.Click += new System.EventHandler(this.lblHeigth_Click);
+            // 
+            // lblHeigth
+            // 
+            this.lblHeigth.AutoSize = true;
+            this.lblHeigth.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeigth.Location = new System.Drawing.Point(52, 282);
+            this.lblHeigth.Name = "lblHeigth";
+            this.lblHeigth.Size = new System.Drawing.Size(492, 29);
+            this.lblHeigth.TabIndex = 2;
+            this.lblHeigth.Text = "The Heigth of the object above the ground is:\r\n";
+            // 
+            // txtHeigth
+            // 
+            this.txtHeigth.Location = new System.Drawing.Point(697, 46);
+            this.txtHeigth.Name = "txtHeigth";
+            this.txtHeigth.Size = new System.Drawing.Size(100, 20);
+            this.txtHeigth.TabIndex = 3;
+            this.txtHeigth.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(706, 294);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(13, 13);
+            this.lblAnswer.TabIndex = 4;
+            this.lblAnswer.Text = "?";
+            // 
+            // FallingObjectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 405);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtHeigth);
+            this.Controls.Add(this.lblHeigth);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "FallingObjectForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FallingObjectForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblHeigth;
+        private System.Windows.Forms.TextBox txtHeigth;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ FallingObjectsOneal/FallingObjects/FallingObjects/FallingObjectForm.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by: Oneal Nnah
+ * Created on: 2 january 2002
+ * Created for: ICS3U Programming
+ * Assignment #2 - FALLING OBJECTS
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingObjects
+{
+    public partial class FallingObjectForm : Form
+    {
+        public FallingObjectForm()
+        {
+            InitializeComponent();
+        }
+
+        private void lblHeigth_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double heigth;
+            heigth = double.Parse(txtHeigth.Text);
+
+            //this is to calculate the height
+            heigth = 100 - (0.5 * 9.81) * Math.Pow(heigth, 2);
+
+            //this is to convert the text to string
+            lblAnswer.Text = Convert.ToString(heigth);
+
+           heigth = double.Parse(txtHeigth.Text);
+             
+            
+             
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FallingObjectForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
